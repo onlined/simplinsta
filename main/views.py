be518acf29ae4140ea146f235a98377dd6dd598c
Ekin Dursun <ekindursun@gmail.com>
2016-05-04 18:16:21 +0300

    Clear code

--- main/views.py
+++ main/views.py
@@ -5,12 +5,15 @@ from django.conf import settings
 
 from .models import Tag
 
+def request_url(tag):
+    return 'https://api.instagram.com/v1/tags/%s/media/recent?count=6&client_id=%s' % (tag, settings.INSTAGRAM_CLIENT_ID)
+
 def show_tag(request, tag=settings.DEFAULT_TAG):
     recent_tags = Tag.objects.all()
-    res = requests.get('https://api.instagram.com/v1/tags/%s/media/recent?count=6&client_id=%s' % (tag, settings.INSTAGRAM_CLIENT_ID))
-    data = res.json()
+    res = requests.get(request_url(tag))
     urls = [[],[]]
     try:
+        data = res.json()
         for i,post in enumerate(data['data']):
             urls[i//3].append(post['images']['low_resolution']['url'])
     except:
