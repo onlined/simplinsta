2d8660d01e948c82cb772dc399572f143d077f90
Ekin Dursun <ekindursun@gmail.com>
2016-05-04 14:06:01 +0300

    Add initial functionality

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.show_tag, name='index'),
     url(r'^([a-zA-Z0-9]*)/$', views.show_tag, name='show_tag'),
-    url(r'^([a-zA-Z0-9]*)/([0-9]*)/$', views.show_tag, name='show_tag_page'),
+    url(r'^([a-zA-Z0-9]*)/prev/$', views.show_tag_prev, name='show_tag_prev'),
+    url(r'^([a-zA-Z0-9]*)/prev/$', views.show_tag_next, name='show_tag_next'),
     url(r'^find/$', views.find_tag, name='find_tag'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import requests
 
-# Create your views here.
+from django.shortcuts import render
+from django.conf import settings
+
+from .models import Tag
+
+def show_tag(request, tag='mektebisultani'):
+    recent_tags = Tag.objects.all()
+    res = requests.get('https://api.instagram.com/v1/tags/%s/media/recent?count=6&client_id=%s' % (tag, settings.INSTAGRAM_CLIENT_ID))
+    data = res.json()
+    urls = [[],[]]
+    try:
+        for i,post in enumerate(data['data']):
+            urls[i//3].append(post['images']['low_resolution']['url'])
+    except:
+        pass
+    return render(request, 'main/show_tag.html', context={
+        'tag': tag,
+        'recent_tags': recent_tags,
+        'urls': urls,
+    })
+    
+def show_tag_prev(request):
+    pass
+    
+def show_tag_next(request):
+    pass
+    
+def find_tag(request):
+    pass
