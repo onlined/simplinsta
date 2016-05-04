6c8ebc11c4174ac91131286c5b2bcd13c9373b76
Ekin Dursun <ekindursun@gmail.com>
2016-05-04 18:15:48 +0300

    Bugfix

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.show_tag, name='index'),
-    url(r'^([a-zA-Z0-9]*)/$', views.show_tag, name='show_tag'),
-    url(r'^([a-zA-Z0-9]*)/prev/$', views.show_tag_prev, name='show_tag_prev'),
-    url(r'^([a-zA-Z0-9]*)/prev/$', views.show_tag_next, name='show_tag_next'),
+    url(r'^(.*)/$', views.show_tag, name='show_tag'),
+    url(r'^(.*)/prev/$', views.show_tag_prev, name='show_tag_prev'),
+    url(r'^(.*)/prev/$', views.show_tag_next, name='show_tag_next'),
     url(r'^find/$', views.find_tag, name='find_tag'),
 ]
